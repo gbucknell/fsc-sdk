deaeaf9e17664fdc98610cdfbd4c2f5fe7667a9e
samuel.debionne <samuel.debionne@8ae27fb2-a4e6-11dd-9bad-6bc74a82fd44>
2009-01-16 16:23:18 +0000

    Initial import.

---
+++ fsc/include/python.hpp
@@ -0,0 +1,24 @@
+//  (c) Copyright 2008 Samuel Debionne.
+//
+//  Distributed under the MIT Software License. (See accompanying file
+//  license.txt) or copy at http://www.opensource.org/licenses/mit-license.php)
+//
+//  See http://code.google.com/p/fsc-sdk/ for the library home page.
+//
+//	$Revision: $
+//	$History: $
+
+
+#if !defined(__PYTHON_HPP__)
+#define __PYTHON_HPP__
+
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+extern "C" void initfsc(void);
+
+
+#endif //__PYTHON_HPP__
