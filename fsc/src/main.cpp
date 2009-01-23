296d5392bd3b653275b28515b469f44431c5161f
samuel.debionne <samuel.debionne@8ae27fb2-a4e6-11dd-9bad-6bc74a82fd44>
2009-01-23 16:21:40 +0000

    Simplify.

--- fsc/src/main.cpp
+++ fsc/src/main.cpp
@@ -14,11 +14,12 @@
 
 #include <iomanip>
 #include <log.hpp>
+#include <worker_thread.hpp>
 #include <cockpit.hpp>
+#include <win/single_instance.hpp>
 #include <win/event.hpp>
 #include <win/exception.hpp>
 #include <fsx/sim_connect.hpp>
-#include <fsx/dispatch_proc.hpp>
 #include <iocards/usb_expansion_card.hpp>
 
 
@@ -30,10 +31,10 @@ extern "C"
 
 #include <vector>
 #include <string>
-#include <boost/array.hpp>
 #include <boost/shared_array.hpp>
 #include <boost/dynamic_bitset.hpp>
 #include <boost/program_options.hpp>
+#include <boost/thread.hpp>
 #include <boost/python.hpp>
 
 #include <gui/init.hpp>
@@ -50,13 +51,22 @@ namespace lg = boost::logging;
 //Global ATL module
 CAppModule _Module;
 
-void init_wtl(HINSTANCE _hInstance);
-void uninit_wtl();
-
 
 //int _tmain(int argc, _TCHAR* argv[])
 int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _lpstrCmdLine, int _nCmdShow)
 {
+    //Initialize GUI components (prior to any call to ATL/WTL)
+    gui::init(_hInstance);
+
+    //Limit applications to one instance
+    win::single_instance instance(L"D3001DBE-4718-43e0-8C70-47802C927165");
+    if (instance.is_already_running())
+    {
+        AtlMessageBox(NULL, IDS_SINGLE_INSTANCE, IDS_SINGLE_INSTANCE_TITLE);
+        //::MessageBox(NULL, L"FSC is limited to a single instance.", L"Already running", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
     //Split command line
     std::vector<std::wstring> args = po::split_winmain(_lpstrCmdLine);
 
@@ -64,6 +74,12 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
     std::wstring cockpit_file;
 
     // Declare the supported options.
+    po::options_description debug_opts("Debug options");
+    debug_opts.add_options()
+        ("no-sim", "do not connect to fsx")
+        ("no-hw", "do not connect to human interface devices")
+        ;
+
     lg::level::type log_level;
     po::options_description other_opts("Other options");
     other_opts.add_options()
@@ -85,7 +101,7 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
     cmd_line_opts.add(other_opts);
 
     po::options_description all_opts;
-    all_opts.add(other_opts).add(cockpit_opts);
+    all_opts.add(debug_opts).add(other_opts).add(cockpit_opts);
 
     po::variables_map vm;
     po::store(po::wcommand_line_parser(args).options(all_opts).positional(p).run(), vm);
@@ -93,9 +109,6 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
 
     //Init logging
     init_logs();
-
-    //Init GUI components
-    gui::init_wtl(_hInstance);
 
     if (! vm.count("nologo"))
     {
@@ -122,24 +135,23 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
 
     try
     {
-        typedef boost::shared_ptr<void> handle_t;
-        handle_t hid_event = win::create_event(TRUE); //Manual reset
-        handle_t simconn_event = win::create_event();
+        win::event_t hid_event = win::create_event(TRUE); //Manual reset
+        win::event_t simconn_event = win::create_event();
+        win::event_t exit_event = win::create_event();
 
         // Open USB expansion
-        iocards::usb_expansion_card usb;
-        if (! usb.open(1, 2, hid_event.get()))
-            throw "Failed to open hardware!";
+        iocards::usb_expansion_card hw;
+        if (! vm.count("no-hw"))
+            if (! hw.open(1, 2, hid_event.get()))
+                throw "Failed to open hardware!";
         
         LINFO_ << "Connected to HID!";
 
-        std::vector<boost::uint8_t> input_buffer(usb.input_size());
-        std::vector<boost::uint8_t> output_buffer(usb.output_size());
-
         //Open SimConnect
-        fsx::sim_connect sc;
-        if (!SUCCEEDED(sc.open("Client Event", NULL, 0, simconn_event.get(), 0)))
-            throw "Failed to connect to Flight Simulator!";
+        fsx::sim_connect sim;
+        if (! vm.count("no-sim"))
+            if (!SUCCEEDED(sim.open("Client Event", NULL, 0, simconn_event.get(), 0)))
+                throw "Failed to connect to Flight Simulator!";
 
         LINFO_ << "Connected to Flight Simulator!";
 
@@ -148,10 +160,10 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
             throw std::runtime_error("Failed to add fsc module to the python interpreter");
 
         //Start the python interpreter
-        Py_Initialize();
-        atexit(Py_Finalize);
+        python::init();
 
         LINFO_ << "Python is initialized!";
+
 /*
         // Get capabilities of the HW
         HIDP_CAPS capabilities = hid.capabilities();
@@ -175,33 +187,43 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
 	    py::object cockpit_class = cokpit_mod.attr("cockpit");
     	
         //Construct the (derived) python cockpit
-	    py::object py_cockpit = cockpit_class("test.cfg");
+	    py::object py_cockpit = cockpit_class(sim, hw, "test.cfg");
         basic_cockpit& cockpit = py::extract<basic_cockpit&>(py_cockpit);
 
+        //basic_cockpit& cockpit = python::make_cockpit(boost::to_utf8(cockpit_file), "test.cfg", sim, hw);
+
         //Call the initialize member function
-	    cockpit.initialize(/*sc*/);
+	    cockpit.initialize();
 
         // Initiate a read from the USB device
-        usb.read(input_buffer);
+        hw.read();
 
 		// Set a notification when the simulation starts so we can get initial value
 		// and set output accordingly.
-        HRESULT hr = sc.subscribe_to_system_event(0, "SimStart");
+        HRESULT hr = sim.subscribe_to_system_event(0, "SimStart");
+
+        //Run the worker thread
+        work w(cockpit, hid_event, simconn_event, exit_event);
+        boost::thread worker(w);
+
+        //Finally run GUI components
+        gui::run();
 
         //Transmit event
-        //hr = sc.transmit_client_event(SIMCONNECT_OBJECT_ID_USER, EVENT_AUTOPILOT_HEADING_BUG_SET, 333, SIMCONNECT_GROUP_PRIORITY_HIGHEST, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
-        //hr = sc.transmit_client_event(SIMCONNECT_OBJECT_ID_USER, EVENT_DME_SELECT, 2, SIMCONNECT_GROUP_PRIORITY_HIGHEST, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+        //hr = sim.transmit_client_event(SIMCONNECT_OBJECT_ID_USER, EVENT_AUTOPILOT_HEADING_BUG_SET, 333, SIMCONNECT_GROUP_PRIORITY_HIGHEST, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+        //hr = sim.transmit_client_event(SIMCONNECT_OBJECT_ID_USER, EVENT_DME_SELECT, 2, SIMCONNECT_GROUP_PRIORITY_HIGHEST, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
 
         //double ThrottlePercent = 80.;
-        //hr = sc.set_data_on_sim_object(DEFINITION_THROTTLE, SIMCONNECT_OBJECT_ID_USER, 0, 0, sizeof(ThrottlePercent), &ThrottlePercent);
-
-        enum EVENT {EVENT_SIMCONNECT, EVENT_HID};
-        boost::array<HANDLE, 2> events;
+        //hr = sim.set_data_on_sim_object(DEFINITION_THROTTLE, SIMCONNECT_OBJECT_ID_USER, 0, 0, sizeof(ThrottlePercent), &ThrottlePercent);
+/*
+        enum EVENT {EVENT_SIMCONNECT, EVENT_HID, EVENT_EXIT};
+        boost::array<HANDLE, 3> events;
         events[EVENT_SIMCONNECT] = simconn_event.get();
 		events[EVENT_HID] = hid_event.get();
+        events[EVENT_EXIT] = exit_event.get();
 
         bool quit = false;
-        context<basic_cockpit> ctxt(quit, cockpit);
+        context<basic_cockpit> ctxt(quit, sim, cockpit);
         while (!quit)
         {
             DWORD EvtId = ::WaitForMultipleObjects(events.size(), events.c_array(), FALSE, INFINITE);
@@ -209,12 +231,12 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
             switch (EvtId)
             {
             case WAIT_OBJECT_0 + EVENT_SIMCONNECT:
-                sc.call_dispatch(dispatch_proc<basic_cockpit>, &quit);
+                sim.call_dispatch(dispatch_proc<basic_cockpit>, &quit);
                 break;
 
             case WAIT_ABANDONED_0 + EVENT_HID:
             case WAIT_TIMEOUT:
-                usb.cancel_io();
+                hw.cancel_io();
                 break;
 
             case WAIT_OBJECT_0 + EVENT_HID:
@@ -229,23 +251,31 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
                     << (int) input_buffer[7] << " " 
                     << (int) input_buffer[8];
                 {
-                DWORD nb_bytes_transferred = usb.overlapped_result();
+                DWORD nb_bytes_transferred = hw.overlapped_result();
 
                 //TODO: Parse HID buffer...
                 //boost::dynamic_bitset<> bitset(res.second * 8);
                 //hid_to_event_mapping bitset
 
                 // Initiate a read
-                DWORD nb_bytes_read = usb.read(input_buffer);
+                DWORD nb_bytes_read = hw.read(input_buffer);
 
-                //hr = sc.transmit_client_event(SIMCONNECT_OBJECT_ID_USER, EVENT_MIXTURE_SET, input_buffer[2] * 0x40, SIMCONNECT_GROUP_PRIORITY_HIGHEST, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+                //hr = sim.transmit_client_event(SIMCONNECT_OBJECT_ID_USER, EVENT_MIXTURE_SET, input_buffer[2] * 0x40, SIMCONNECT_GROUP_PRIORITY_HIGHEST, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
                 }
+                break;
+
+            case WAIT_OBJECT_0 + EVENT_EXIT:
+                quit = true;
                 break;
 
             default :
                 LERR_ << "unexpected event: " << EvtId;
             }
         }
+*/
+        //End the worker thread
+        win::set(exit_event);
+        worker.join();
     }
     catch(char* _ex)
     {
@@ -264,11 +294,10 @@ int WINAPI _tWinMain(HINSTANCE _hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR _
     }
     catch(py::error_already_set const &)
     {
-        // TODO redirect stderr to LERR_.
-        PyErr_Print();
+        LERR_ << python::get_error();
     };
-
-    gui::uninit_wtl();
+    
+    gui::uninit();
 
     return 0;
 }
