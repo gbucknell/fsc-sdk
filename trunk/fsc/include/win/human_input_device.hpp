ba4122a9b62c5edf9a533c3bcafb35c98a4426f6
samuel.debionne <samuel.debionne@8ae27fb2-a4e6-11dd-9bad-6bc74a82fd44>
2009-01-23 16:13:21 +0000

    Make human_input_device copiable.

--- trunk/fsc/include/win/human_input_device.hpp
+++ trunk/fsc/include/win/human_input_device.hpp
@@ -29,7 +29,9 @@ extern "C" {
 
 #include <boost/utility.hpp>
 #include <boost/shared_ptr.hpp>
+
 #include <win/exception.hpp>
+#include <assert.hpp>
 
 
 #define HID_PRECONDITION(x) _ASSERT((x))
@@ -39,14 +41,21 @@ namespace win {
 
 
 /// HID object. Encapuslate a Human Input Device handle.
-class human_input_device : public boost::noncopyable
+class human_input_device //: public boost::noncopyable
 {
+    typedef boost::shared_ptr<void> handle_t;
+
 public:
-    human_input_device() : hid_(INVALID_HANDLE_VALUE) {memset(&overlapped_, 0, sizeof(OVERLAPPED));}
-    ~human_input_device() {if (hid_ != INVALID_HANDLE_VALUE) ::CloseHandle(hid_);}
+    human_input_device() {memset(&overlapped_, 0, sizeof(OVERLAPPED));}
 
     /// Open an HID with a given vendor id and product id.
     BOOL open(USHORT _VendorId, USHORT _ProductId, HANDLE _hOverlapped);
+
+    /// Close the HID
+    BOOL close() {hid_.reset(); }
+
+    /// Return true is the human_input_device object is currently opened, false otherwise.
+    bool is_open() {return hid_;}
 
     /// Return information about a top-level collection's capability.
     HIDP_CAPS capabilities() const;
@@ -58,15 +67,14 @@ public:
     BOOL cancel_io() const
     {
         HID_PRECONDITION(hid_ != NULL);
-        return ::CancelIo(hid_);
+        return ::CancelIo(handle());
     }
 
     /// Reads data from the device.
     DWORD read(LPVOID _lpBuffer, DWORD _dwNumberOfBytesToRead)
     {
-        HID_PRECONDITION(hid_ != NULL);
         DWORD number_of_bytes_read = 0;
-        if (! ::ReadFile(hid_, _lpBuffer, _dwNumberOfBytesToRead, &number_of_bytes_read, &overlapped_))
+        if (! ::ReadFile(handle(), _lpBuffer, _dwNumberOfBytesToRead, &number_of_bytes_read, &overlapped_))
         {
             DWORD err = ::GetLastError();
             if (err != ERROR_IO_PENDING)
@@ -78,9 +86,8 @@ public:
     /// Write data to the device.
     DWORD write(LPVOID _lpBuffer, DWORD _dwNumberOfBytesToWrite)
     {
-        HID_PRECONDITION(hid_ != NULL);
         DWORD number_of_bytes_written = 0;
-        if (! ::WriteFile(hid_, _lpBuffer, _dwNumberOfBytesToWrite, &number_of_bytes_written, &overlapped_))
+        if (! ::WriteFile(handle(), _lpBuffer, _dwNumberOfBytesToWrite, &number_of_bytes_written, &overlapped_))
         {
             DWORD err = ::GetLastError();
             if (err != ERROR_IO_PENDING)
@@ -92,15 +99,21 @@ public:
     DWORD overlapped_result(BOOL _bWait = FALSE)
     {
         DWORD number_of_bytes_transferred = 0;
-        if (! ::GetOverlappedResult(hid_, &overlapped_, &number_of_bytes_transferred, _bWait))
+        if (! ::GetOverlappedResult(handle(), &overlapped_, &number_of_bytes_transferred, _bWait))
             throw runtime_error("GetOverlappedResult failed");
         return number_of_bytes_transferred;
     }
 
 
 private:
-    HANDLE hid_;
+    handle_t hid_;
     OVERLAPPED overlapped_;
+
+    HANDLE handle() const
+    {
+        FSC_PRECONDITION((bool) hid_);
+        return hid_.get();
+    }
 };
 
 
